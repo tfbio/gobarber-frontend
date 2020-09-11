e7807b0bf7a0104552e9959e10558b3840fd9485
Fabio Oliveira <fabiotavares19@hotmail.com>
2020-09-11 10:04:28 -0300

    Dashboard header added

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,28 +2,31 @@ import React from 'react';
 import { FiPower } from 'react-icons/fi';
 import { Container, Header, HeaderContent, Profile } from './styles';
 
+import { useAuth } from '../../hooks/authContext';
+
 import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const { logout } = useAuth();
   return (
     <Container>
       <Header>
         <HeaderContent>
           <img src={logo} alt="GoBarber" />
+          <Profile>
+            <img
+              src="https://avatars0.githubusercontent.com/u/61916772?s=460&u=e0be4ed8cb4979ef573730ff81d28666887aa924&v=4"
+              alt="Profile"
+            />
+            <div>
+              <span>Welcome,</span>
+              <strong>Fabio Oliveira</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={logout}>
+            <FiPower />
+          </button>
         </HeaderContent>
-        <Profile>
-          <img
-            src="https://avatars0.githubusercontent.com/u/61916772?s=460&u=e0be4ed8cb4979ef573730ff81d28666887aa924&v=4"
-            alt="Profile"
-          />
-          <div>
-            <span>Welcome,</span>
-            <strong>Fabio Oliveira</strong>
-          </div>
-        </Profile>
-        <button type="button">
-          <FiPower />
-        </button>
       </Header>
     </Container>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,58 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
-export const Header = styled.header``;
-export const HeaderContent = styled.div``;
-export const Profile = styled.div``;
+
+export const Header = styled.header`
+  padding: 20px 0%;
+  background: #28262e;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+
+  > img {
+    height: 80px;
+  }
+
+  button {
+    margin-left: auto;
+    background: transparent;
+    border: 0;
+
+    svg {
+      color: #999591;
+      width: 24px;
+      height: 24px;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 80px;
+
+  img {
+    width: 58px;
+    height: 58px;
+    border-radius: 50%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+  }
+
+  span {
+    color: #f4ede8;
+  }
+
+  strong {
+    color: #ff9000;
+  }
+`;
